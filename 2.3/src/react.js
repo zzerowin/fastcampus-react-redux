168f5a4de993c4bd67d8756e2773ce15f3317705
zzerowin <alaykr123@gmail.com>
2022-03-01 12:16:35 +0900

    react last

--- 2.3/src/react.js
+++ 2.3/src/react.js
@@ -1,3 +1,5 @@
+const hooks = [];
+let currentComponent = 0;
 
 export class Component {
   constructor(props) {
@@ -5,17 +7,18 @@ export class Component {
   }
 }
 
-export function createDOM(node) {
-  if (typeof node === 'string') {
+function createDOM(node) {
+  if (typeof node === 'string' || typeof node === 'number') {
     return document.createTextNode(node);
   }
 
   const element = document.createElement(node.tag);
 
-  Object.entries(node.props)
+  node.props && Object
+      .entries(node.props)
       .forEach(([name, value]) => element.setAttribute(name, value));
 
-  node.children
+  node.children && node.children
       .map(createDOM)
       .forEach(element.appendChild.bind(element));
 
@@ -25,30 +28,54 @@ export function createDOM(node) {
 function makeProps(props, children) {
   return {
     ...props,
-    children: children.length === 1 ? children[0] : children,
+    children: children.length === 1 ? children[0] : children
+  }
+}
+
+function useState(initValue) {
+  let position = currentComponent - 1;
+
+  if (!hooks[position]) {
+    hooks[position] = initValue;
+  }
+
+  const modifier = nextValue => {
+    hooks[position] = nextValue;
   };
+
+  return [hooks[position], modifier];
 }
 
 export function createElement(tag, props, ...children) {
-  props = props || {};
-
   if (typeof tag === 'function') {
     if (tag.prototype instanceof Component) {
       const instance = new tag(makeProps(props, children));
       return instance.render();
-    } else {
-      if (children.length > 0) {
-        return tag(makeProps(props, children))
-      } else {
-        return tag(props);
-      }
     }
 
-  } else {
-    return { tag, props, children };
+    hooks[currentComponent] = null;
+    currentComponent++;
+
+    if (children.length > 0) {
+      return tag(makeProps(props, children));
+    } else {
+      return tag(props);
+    }
   }
+
+  return { tag, props, children };
 }
 
-export function render(vdom, container) {
-  container.appendChild(createDOM(vdom));
-}
+export const render = (function() {
+  let prevDom = null;
+
+  return function(vdom, container) {
+    if (prevDom === null) {
+      prevDom = vdom;
+    }
+
+    // diff
+
+    container.appendChild(createDOM(vdom));
+  }
+})();
